c1597da9a01e5d7ca794a8c8f2ab75165fda1282
arne182 <arneschwarck@gmail.com>
2019-03-09 19:05:25 +0100

    Update planner.py

--- selfdrive/controls/lib/planner.py
+++ selfdrive/controls/lib/planner.py
@@ -500,7 +500,10 @@ class Planner(object):
       try:
         LongitudinalMpc.get_relative_velocity(self.lead_1.vRel)
       except:
-        LongitudinalMpc.get_relative_velocity(0)
+        try:
+         LongitudinalMpc.get_relative_velocity(0)
+        except TypeError:
+         none
 
       enabled = (LoC.long_control_state == LongCtrlState.pid) or (LoC.long_control_state == LongCtrlState.stopping)
       following = self.lead_1.status and self.lead_1.dRel < 45.0 and self.lead_1.vLeadK > CS.vEgo and self.lead_1.aLeadK > 0.0
