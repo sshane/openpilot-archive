e29c3eb6f1d39ee6bd723a4afd3af45db52900eb
Shane <shane@smiskol.com>
2019-06-05 13:10:02 -0500

    braking tuning from comma stock

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -87,15 +87,15 @@ class CarInterface(object):
     ret.startAccel = 0.0
 
     if ret.enableGasInterceptor:
-      ret.gasMaxBP = [0., 9., 55]
+      ret.gasMaxBP = [0., 9., 35]
       ret.gasMaxV = [0.2, 0.5, 0.7]
-      ret.longitudinalTuning.kpV = [2.0, 0.3, 0.15]  # braking tune
-      ret.longitudinalTuning.kiV = [0.2, 0.1]
+      ret.longitudinalTuning.kpV = [1.2, 0.8, 0.5]
+      ret.longitudinalTuning.kiV = [0.18, 0.12]
     else:
       ret.gasMaxBP = [0.]
       ret.gasMaxV = [0.5]
-      ret.longitudinalTuning.kpV = [1.0, 0.5, 0.3]  # braking tune from rav4h
-      ret.longitudinalTuning.kiV = [0.20, 0.10]
+      ret.longitudinalTuning.kpV = [3.6, 2.4, 1.5]
+      ret.longitudinalTuning.kiV = [0.54, 0.36]
 
     ret.steerActuatorDelay = 0.12  # Default delay, Prius has larger delay
     if candidate != CAR.PRIUS:
