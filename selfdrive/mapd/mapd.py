b109d3b973f7ab4172b1423cd18f760c58ff5c79
Arne Schwarck <arneschwarck@gmail.com>
2019-06-13 14:22:00 +0200

    add cur_way.

--- selfdrive/mapd/mapd.py
+++ selfdrive/mapd/mapd.py
@@ -292,7 +292,7 @@ def mapsd_thread():
           max_speed_ahead = ahead_speed.max_speed()
           print "speed ahead found"
           print max_speed_ahead
-          max_speed_ahead_dist = distance_to_closest_node(lat, lon, heading, pnts)
+          max_speed_ahead_dist = cur_way.distance_to_closest_node(lat, lon, heading, pnts)
           
         if abs(max_speed - max_speed_prev) > 0.1:
           speedLimittrafficvalid = False
