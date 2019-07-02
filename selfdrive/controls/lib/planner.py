49af82e7deb3568fe6999eecba7a68a0c44efe3a
Arne Schwarck <arneschwarck@gmail.com>
2019-07-02 20:25:46 +0200

    reduce the deceleration by half

--- selfdrive/controls/lib/planner.py
+++ selfdrive/controls/lib/planner.py
@@ -224,13 +224,13 @@ class Planner(object):
       # Change accel limits based on time remaining to turn
       if decel_for_turn:
         time_to_turn = max(1.0, live_map_data.liveMapData.distToTurn / max((v_ego + v_curvature)/2, 1.))
-        required_decel = min(0, (v_curvature - v_ego) / time_to_turn)
+        required_decel = min(0, (v_curvature - v_ego) / time_to_turn/2)
         accel_limits[0] = max(accel_limits[0], required_decel)
         
       if v_speedlimit_ahead < v_speedlimit:
         if live_map_data.liveMapData.speedLimitAheadDistance != 0:
           required_decel = min(0, (v_speedlimit_ahead*v_speedlimit_ahead - v_ego*v_ego)/(live_map_data.liveMapData.speedLimitAheadDistance*2))
-        required_decel = max(required_decel, -3.0)
+        required_decel = max(required_decel/2, -3.0)
         #print "required_decel"
         #print required_decel
         #print "accel_limits 0"
