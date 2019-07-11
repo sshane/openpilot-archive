96ff5ec596036969d67545edb89ee3dc34e7e020
Shane <shane@smiskol.com>
2019-07-10 21:19:16 -0500

    test fix

--- selfdrive/df/test.py
+++ selfdrive/df/test.py
@@ -2,14 +2,14 @@ import time
 from selfdrive.df import df_wrapper
 df_model = df_wrapper.get_wrapper()
 df_model.init_model()
-start = time.time()
+'''start = time.time()
 for i in range(30):
  model_output = df_model.run_model(0.8653078153514447,
   0.46805728618371273,
   0.46805728618371273,
   0.28780443294609244,
   0.01075646532123655)
-print(time.time() - start)
+print(time.time() - start)'''
 model_output = df_model.run_model(.5, .2, .1)
 print(model_output)
 model_output = df_model.run_model(.2, .3, .4)
