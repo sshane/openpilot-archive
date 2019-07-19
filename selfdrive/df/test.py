e279019d75caf645353d9346c5ec0d430b72daf8
Shane <shane@smiskol.com>
2019-07-19 06:27:37 -0500

    test

--- selfdrive/df/test.py
+++ selfdrive/df/test.py
@@ -14,7 +14,7 @@ print(time.time() - startl)'''
 input_data = [[.8, .8, .6]]
 for i in range(29):
     input_data.append([input_data[-1][0]-.013, input_data[-1][1], input_data[-1][2]-.01])
-
+input_data = sum(input_data, [])
 pred = df_model.run_model_lstm(input_data)
 print(pred)
 '''model_output = df_model.run_model(.5, .5, .2, .1, .5)
