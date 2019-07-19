799427266dd78bcd8a4f1a19efce347d5fb75c4e
Shane <shane@smiskol.com>
2019-07-19 05:39:20 -0500

    test

--- selfdrive/df/dynamic_follow.cc
+++ selfdrive/df/dynamic_follow.cc
@@ -86,9 +86,9 @@ extern "C" {
   std::array<float, 3> texts = {0.0, 1.5, 9.5};
     // ^ An array of 3 elements with the type std::string
 
-  //for(const auto& float : texts) {   // Range-for!
-      //std::cout << text << std::endl;
-  //}
+  for(const auto& float : texts) {   // Range-for!
+      std::cout << text << std::endl;
+  }
   }
 
 int main(){
