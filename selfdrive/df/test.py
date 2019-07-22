b512a9cad7b9cf48772637f2f25b99e0d4d5b1e2
Shane <shane@smiskol.com>
2019-07-22 17:55:01 -0500

    test

--- selfdrive/df/test.py
+++ selfdrive/df/test.py
@@ -11,15 +11,15 @@ for i in range(30):
   0.01075646532123655)
 print(time.time() - startl)'''
 
-input_data = [[.6, .6, .55]]
+'''input_data = [[.6, .6, .55]]
 for i in range(29):
     input_data.append([input_data[-1][0]-.01, input_data[-1][1], input_data[-1][2]-.001])
 input_data = sum(input_data, [])
 pred = df_model.run_model_lstm(input_data)
-print(pred)
-'''model_output = df_model.run_model(.5, .5, .2, .1, .5)
+print(pred)'''
+model_output = df_model.run_model(.5, .2, .1, .5)
 print(model_output)
-model_output = df_model.run_model(.05, .5, .3, .4, .6)
+model_output = df_model.run_model(.05, .3, .4, .6)
 print(model_output)
-model_output = df_model.run_model(.7, .5, .85, .55, .5)
-print(model_output)'''
+model_output = df_model.run_model(.7, .85, .55, .5)
+print(model_output)
