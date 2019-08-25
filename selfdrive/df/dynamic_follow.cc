e20604fd69acb7e17569bde8b10d37b29008ef28
Shane <shane@smiskol.com>
2019-08-24 22:30:58 -0500

    test

--- selfdrive/df/dynamic_follow.cc
+++ selfdrive/df/dynamic_follow.cc
@@ -87,6 +87,7 @@ extern "C" {
     int size = 60;
     std::vector<float> inputVec;
     for (int i = 0; i < size; i++ ) {
+      std::cout << inputData[i];
       inputVec.push_back(inputData[i]);
     }
 
