b7f02f65ec86f368b810a5d9ff250083d8ba87b2
Ross Fisher <zorrobyte@gmail.com>
2019-08-29 23:33:02 -0400

    Revert "Update interface.py"
    This reverts commit d42c0197a94ac51b90d665d04b69988d9b9afb63.

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -56,7 +56,7 @@ class CarInterface(object):
 
     ret.steerActuatorDelay = 0.12  # Default delay, Prius has larger delay
 
-    if candidate not in [CAR.RAV4, CAR.RAV4H]: # These cars use LQR/INDI
+    if candidate not in [CAR.PRIUS, CAR.RAV4, CAR.RAV4H]: # These cars use LQR/INDI
       ret.lateralTuning.init('pid')
       ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0.], [0.]]
 
@@ -68,8 +68,25 @@ class CarInterface(object):
       tire_stiffness_factor = 0.725   # hand-tune
       ret.mass = 3375. * CV.LB_TO_KG + STD_CARGO_KG
 
-      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.6], [0.1]]
-      ret.lateralTuning.pid.kf = 0.00006   # full torque for 10 deg at 80mph means 0.00007818594
+      #ret.lateralTuning.init('indi')
+      #ret.lateralTuning.indi.innerLoopGain = 4.0
+      #ret.lateralTuning.indi.outerLoopGain = 3.0
+      #ret.lateralTuning.indi.timeConstant = 2.0
+      #ret.lateralTuning.indi.actuatorEffectiveness = 1.0
+
+      # TODO: Determine if this is better than INDI
+      ret.lateralTuning.init('lqr')
+      ret.lateralTuning.lqr.scale = 1500.0
+      ret.lateralTuning.lqr.ki = 0.01
+
+      ret.lateralTuning.lqr.a = [0., 1., -0.22619643, 1.21822268]
+      ret.lateralTuning.lqr.b = [-1.92006585e-04, 3.95603032e-05]
+      ret.lateralTuning.lqr.c = [1., 0.]
+      ret.lateralTuning.lqr.k = [-110.73572306, 451.22718255]
+      ret.lateralTuning.lqr.l = [0.03233671, 0.03185757]
+      ret.lateralTuning.lqr.dcGain = 0.002237852961363602
+
+      ret.steerActuatorDelay = 0.5
 
     elif candidate in [CAR.RAV4, CAR.RAV4H]:
       stop_and_go = True if (candidate in CAR.RAV4H) else False
