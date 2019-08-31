710e6795aaeda62ee4825003193a0039db3f3977
Shane <shane@smiskol.com>
2019-08-30 19:33:44 -0500

    forgot to normalize, extrapolate above ranges linearly

--- selfdrive/controls/lib/latcontrol_indi.py
+++ selfdrive/controls/lib/latcontrol_indi.py
@@ -10,6 +10,8 @@ from selfdrive.controls.lib.drive_helpers import get_steer_max
 from selfdrive.virtualZSS import virtualZSS_wrapper
 from selfdrive.kegman_conf import kegman_conf
 
+def interp(x, xp, fp):  # extrapolates above range, np.interp does not
+  return (((x - xp[0]) * (fp[1] - fp[0])) / (xp[1] - xp[0])) + fp[0]
 
 def mean(numbers):
   return float(sum(numbers)) / max(len(numbers), 1)
@@ -57,6 +59,8 @@ class LatControlINDI(object):
     self.reset()
     self.past_data = []
     self.seq_len = 20
+    self.scales = {'zorro_sensor': [-31.666841506958008, 39.42588806152344],
+                   'stock_sensor': [-31.0, 37.599998474121094], 'steer_command': [-1.0, 1.0]}
 
   def reset(self):
     self.delayed_output = 0.
@@ -88,12 +92,12 @@ class LatControlINDI(object):
   def update(self, active, v_ego, angle_steers, angle_steers_rate, eps_torque, steer_override, CP, VM, path_plan, driver_torque):
 
     #virtualZSS
-    self.past_data.append([angle_steers, self.output_steer])
+    self.past_data.append([interp(angle_steers, self.scales['stock_sensor'], [0, 1]), self.output_steer])  # steer command is already 'normalized'
     while len(self.past_data) > self.seq_len:
       del self.past_data[0]
 
     if len(self.past_data) == self.seq_len:
-      angle_steers = float(self.model_wrapper.run_model_time_series([i for x in self.past_data for i in x]))
+      angle_steers = interp(float(self.model_wrapper.run_model_time_series([i for x in self.past_data for i in x])), [0.0, 1.0], self.scales['zorro_sensor'])
 
     # smooth angle
     #max_samples = 20
