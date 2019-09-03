18e381275d4146dfe8c5495ff1641f1270f8d886
Ross Fisher <zorrobyte@gmail.com>
2019-09-02 21:13:12 -0400

    Update lane_planner.py

--- selfdrive/controls/lib/lane_planner.py
+++ selfdrive/controls/lib/lane_planner.py
@@ -31,9 +31,9 @@ class LanePlanner(object):
     self.p_poly = [0., 0., 0., 0.]
     self.d_poly = [0., 0., 0., 0.]
 
-    self.lane_width_estimate = 3.7
-    self.lane_width_certainty = 1.0
-    self.lane_width = 3.7
+    self.lane_width = 3.0
+    self.readings = []
+    self.frame = 0
 
     self.l_prob = 0.
     self.r_prob = 0.
