62520549cf489d4304c63b3dbaea10bde11c9fd8
Shane <shane@smiskol.com>
2019-09-19 10:24:13 -0500

    add gyro data

--- selfdrive/controls/lib/long_mpc.py
+++ selfdrive/controls/lib/long_mpc.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 import time
+from selfdrive.services import service_list
 
 import selfdrive.messaging as messaging
 from selfdrive.swaglog import cloudlog
@@ -30,6 +31,7 @@ class LongitudinalMpc(object):
     self.df_data = []
     self.df_frame = 0
     #self.last_live_tracks = []
+    self.sensor = messaging.sub_sock(service_list['sensorEvents'].port)
 
   def send_mpc_solution(self, qp_iterations, calculation_time):
     qp_iterations = max(0, qp_iterations)
@@ -75,6 +77,12 @@ class LongitudinalMpc(object):
     left_blinker = CS.leftBlinker
     right_blinker = CS.rightBlinker
     set_speed = CS.cruiseState.speed
+    gyro = []
+    sensors = messaging.recv_sock(self.sensor)
+    if sensors is not None:
+      for sensor in sensors.sensorEvents:
+        if sensor.type == 4:  # gyro
+          gyro = list(sensor.gyro.v)
 
     # Setup current mpc state
     self.cur_state[0].x_ego = 0.0
@@ -100,7 +108,7 @@ class LongitudinalMpc(object):
       self.df_data.append({'v_ego': v_ego, 'a_ego': a_ego, 'v_lead': v_lead, 'status': lead_status, 'x_lead': x_lead, 'y_lead': y_lead,
                            'a_lead': a_lead, 'a_rel': a_rel, 'v_lat': v_lat, 'steer_angle': steer_angle, 'steer_rate': steer_rate,
                            'path_curvature': path_curvature, 'live_tracks': track_data, 'time': time.time(), 'gas': gas, 'brake': brake,
-                           'car_gas': car_gas, 'left_blinker': left_blinker, 'right_blinker': right_blinker, 'decel_for_model': decel_for_model, 'set_speed': set_speed})
+                           'car_gas': car_gas, 'left_blinker': left_blinker, 'right_blinker': right_blinker, 'decel_for_model': decel_for_model, 'set_speed': set_speed, 'gyro': gyro})
       if self.df_frame >= 800:  # every 20 seconds, write to file
         try:
           with open("/data/openpilot/selfdrive/df_dc/df-data", "a") as f:
