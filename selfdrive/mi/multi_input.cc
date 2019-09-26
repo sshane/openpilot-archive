a31d79587672397070d31abe598ed3294340c6e2
Shane <shane@smiskol.com>
2019-09-26 12:25:11 -0500

    test

--- selfdrive/mi/multi_input.cc
+++ selfdrive/mi/multi_input.cc
@@ -38,8 +38,8 @@ void initializeSNPE(zdl::DlSystem::Runtime_t runtime) {
 
 void getInputTensor1(std::unique_ptr<zdl::SNPE::SNPE> &snpe, std::vector<float> inputVec) {
   std::unique_ptr<zdl::DlSystem::ITensor> input;
-  /*const auto &strList_opt = snpe->getInputTensorNames();
-  if (!strList_opt) throw std::runtime_error("Error obtaining Input tensor names");
+  const auto &strList_opt = snpe->getInputTensorNames();
+  /*if (!strList_opt) throw std::runtime_error("Error obtaining Input tensor names");
   const auto &strList = *strList_opt;
   std::cout << "input shape: " << inputShape << "\n";
   const auto &inputDims_opt = snpe->getInputDimensions(strList.at(0));
