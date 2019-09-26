b110f8f6533ff146e3296ae86b7ac92d4a3cc986
Shane <shane@smiskol.com>
2019-09-26 11:56:16 -0500

    i'm so dumb

--- selfdrive/mi/multi_input.cc
+++ selfdrive/mi/multi_input.cc
@@ -10,7 +10,7 @@ zdl::DlSystem::Runtime_t checkRuntime()
     static zdl::DlSystem::Version_t Version = zdl::SNPE::SNPEFactory::getLibraryVersion();
     static zdl::DlSystem::Runtime_t Runtime;
     std::cout << "SNPE Version: " << Version.asString().c_str() << std::endl; //Print Version number
-    printf("got runtime");
+    std::cout << "test";
     if (zdl::SNPE::SNPEFactory::isRuntimeAvailable(zdl::DlSystem::Runtime_t::GPU)) {
         Runtime = zdl::DlSystem::Runtime_t::GPU;
     } else {
