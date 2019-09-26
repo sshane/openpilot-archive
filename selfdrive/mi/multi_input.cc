39c45c6106700c73074c63b2bd025ba8accadf07
Shane <shane@smiskol.com>
2019-09-26 11:54:58 -0500

    i'm so dumb

--- selfdrive/mi/multi_input.cc
+++ selfdrive/mi/multi_input.cc
@@ -21,7 +21,7 @@ zdl::DlSystem::Runtime_t checkRuntime()
 
 void initializeSNPE(zdl::DlSystem::Runtime_t runtime) {
   std::unique_ptr<zdl::DlContainer::IDlContainer> container;
-  container = zdl::DlContainer::IDlContainer::open("/data/openpilot/selfdrive/df/multi_input.dlc");
+  container = zdl::DlContainer::IDlContainer::open("/data/openpilot/selfdrive/mi/multi_input.dlc");
   //printf("loaded model\n");
   int counter = 0;
   zdl::SNPE::SNPEBuilder snpeBuilder(container.get());
