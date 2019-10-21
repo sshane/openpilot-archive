1f7b34b26f0aad2b7d0aec5bb272c50dfffa6205
Shane <shane@smiskol.com>
2019-10-21 15:53:35 -0500

    remove pycharm formatting

--- op_edit.py
+++ op_edit.py
@@ -7,13 +7,10 @@ def op_edit():  # use by running `python /data/openpilot/op_edit.py`
   params = op_params.get()
   print('Welcome to the opParams command line editor!')
   print('Here are your parameters:\n')
-  values_list = [
-    params[i] if len(str(params[i])) < 20 else '{} ... {}'.format(str(params[i])[:30], str(params[i])[-15:]) for i in
-    params]
+  values_list = [params[i] if len(str(params[i])) < 20 else '{} ... {}'.format(str(params[i])[:30], str(params[i])[-15:]) for i in params]
   while True:
     params = op_params.get()
-    print('\n'.join(['{}. {}: {} ({})'.format(idx + 1, i, values_list[idx], str(type(params[i]))[8:-2]) for idx, i in
-                     enumerate(params)]))
+    print('\n'.join(['{}. {}: {} ({})'.format(idx + 1, i, values_list[idx], str(type(params[i]))[8:-2]) for idx, i in enumerate(params)]))
     print('\nChoose a parameter to edit (by index): ')
     choice = input('>> ')
     try:
@@ -31,8 +28,7 @@ def op_edit():  # use by running `python /data/openpilot/op_edit.py`
     new_value = input('>> ')
     try:
       new_value = ast.literal_eval(new_value)
-      print('New value: {} ({})\nOld value: {} ({})'.format(new_value, str(type(new_value))[8:-2], old_value,
-                                                            str(type(old_value))[8:-2]))
+      print('New value: {} ({})\nOld value: {} ({})'.format(new_value, str(type(new_value))[8:-2], old_value, str(type(old_value))[8:-2]))
       print('Do you want to save this?')
       choice = input('[Y/n]: ').lower()
       if choice == 'y':
