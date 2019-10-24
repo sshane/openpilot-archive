39b6a2c76398d6825a48e7594490027c6c8bec5f
Shane <shane@smiskol.com>
2019-10-23 19:13:48 -0500

    new model

--- selfdrive/df/dynamic_follow.h
+++ selfdrive/df/dynamic_follow.h
@@ -17,7 +17,7 @@ extern "C"{
   float run_model(float v_ego, float a_ego, float v_lead, float x_lead, float a_lead);
   void init_model();
   float run_model_lstm(float inputData[60]);
-  float run_model_live_tracks(float inputData[54]);
+  float run_model_live_tracks(float inputData[62]);
 }
 
 struct testvar_t {
