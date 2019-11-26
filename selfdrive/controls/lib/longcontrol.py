58cb3c29b15f281490a4cc9ecc72897131544d93
Shane <shane@smiskol.com>
2019-11-26 00:19:35 -0600

    tuning for long control

--- selfdrive/controls/lib/longcontrol.py
+++ selfdrive/controls/lib/longcontrol.py
@@ -60,7 +60,7 @@ class LongControl():
     self.long_control_state = LongCtrlState.off  # initialized to off
     self.pid = PIController((CP.longitudinalTuning.kpBP, CP.longitudinalTuning.kpV),
                             (CP.longitudinalTuning.kiBP, CP.longitudinalTuning.kiV),
-                            k_f=0.75,
+                            k_f=1.15,
                             rate=RATE,
                             sat_limit=0.8,
                             convert=compute_gb)
