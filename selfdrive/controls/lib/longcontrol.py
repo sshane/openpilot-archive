be58c12edf4530bc8d36397a92dc4e25360920df
Shane Smiskol <shane@smiskol.com>
2019-12-19 21:33:09 -0600

    don't reset tracks before next update, duh! fix one car reporting more than 1 point, eg. semi trucks

--- selfdrive/controls/lib/longcontrol.py
+++ selfdrive/controls/lib/longcontrol.py
@@ -142,18 +142,25 @@ class LongControl():
     min_tracks = 3
     track_speed_margin = .7  # 70 percent
     MPC_TIME_STEP = 1 / 20.
+    similar_track_tolerance = 0.025
     if self.dynamic_lane_speed_active and self.v_ego > self.min_dynamic_lane_speed:
-      self.track_data = [trk_vel for trk_vel in self.track_data if (self.v_ego * track_speed_margin) <= trk_vel <= v_cruise]
-      if len(self.track_data) >= min_tracks:
-        average_track_speed = np.mean(self.track_data)
+      tracks = []
+      for trk_vel in self.track_data:
+        valid = all([True if abs(x - trk_vel) > similar_track_tolerance else False for x in tracks])  # radar sometimes reports multiple points for one vehicle, especially semis
+        # todo: factor in yRel, so multiple vehicles that happen to be at the exact same speed aren't filtered out!
+        if valid:
+          tracks.append(trk_vel)
+      tracks = [trk_vel for trk_vel in tracks if (self.v_ego * track_speed_margin) <= trk_vel <= v_cruise]  # .125, 0.025, 0.02500009536743164, 0.02500009536743164
+      if len(tracks) >= min_tracks:
+        average_track_speed = np.mean(tracks)
         if average_track_speed < v_target and average_track_speed < v_target_future:
           # so basically, if there's at least 3 tracks, the speeds of the tracks must be within n% of set speed, if our speed is at least set_speed mph,
           # if the average speeds of tracks is less than v_target and v_target_future, then get a weight for how many tracks exist, with more tracks, the more we
           # favor the average track speed, then weighted average it with our set_speed, if these conditions aren't met, then we just return original values
           # this should work...?
-          x = [3, 6, 16]
-          y = [0.25, .35, 0.45]
-          track_speed_weight = interp(len(self.track_data), x, y)
+          x = [3, 6, 19]
+          y = [0.275, .375, 0.5]
+          track_speed_weight = interp(len(tracks), x, y)
           if self.lead_data['status']:  # if lead, give more weight to surrounding tracks (todo: this if check might need to be flipped, so if not lead...)
             track_speed_weight = clip(1.05 * track_speed_weight, min(y), max(y))
           v_target_slow = (v_cruise * (1 - track_speed_weight)) + (average_track_speed * track_speed_weight)
