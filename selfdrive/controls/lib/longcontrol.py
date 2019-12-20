0bbba3d70552553dde37eb681ee5aa6eea52bca5
Shane Smiskol <shane@smiskol.com>
2019-12-19 18:04:36 -0600

    tuning

--- selfdrive/controls/lib/longcontrol.py
+++ selfdrive/controls/lib/longcontrol.py
@@ -152,7 +152,7 @@ class LongControl():
           # favor the average track speed, then weighted average it with our set_speed, if these conditions aren't met, then we just return original values
           # this should work...?
           x = [3, 6, 16]
-          y = [0.325, .525, 0.625]
+          y = [0.25, .35, 0.45]
           track_speed_weight = interp(len(self.track_data), x, y)
           if self.lead_data['status']:  # if lead, give more weight to surrounding tracks (todo: this if check might need to be flipped, so if not lead...)
             track_speed_weight = clip(1.05 * track_speed_weight, min(y), max(y))
