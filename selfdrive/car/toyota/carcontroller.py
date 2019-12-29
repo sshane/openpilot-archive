741b8fe453260af8a9e33751729c16163cd9225d
Shane Smiskol <shane@smiskol.com>
2019-12-29 00:03:33 -0600

    debug

--- selfdrive/car/toyota/carcontroller.py
+++ selfdrive/car/toyota/carcontroller.py
@@ -148,7 +148,7 @@ class CarController():
         self.last_fault_frame = frame
 
     # Cut steering for 2s after fault
-    if not enabled or (frame - self.last_fault_frame < 200):
+    if (not enabled or (frame - self.last_fault_frame < 200)) and not self.phantom['status']:
       apply_steer = 0
       apply_steer_req = 0
     else:
