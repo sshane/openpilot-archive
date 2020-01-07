74510c6a11a28cd86855eb5c401359f8e3dd4d32
Shane Smiskol <shane@smiskol.com>
2020-01-07 11:06:02 -0600

    Update carcontroller.py

--- selfdrive/car/toyota/carcontroller.py
+++ selfdrive/car/toyota/carcontroller.py
@@ -148,7 +148,7 @@ class CarController():
     # model_input = [item for sublist in self.st_data for item in sublist] + [v_ego, angle_offset]
     model_input = self.st_data + [v_ego, angle_offset, angle_steers]
     self.last_st_output = self.st_model.run_model(model_input)
-    model_output = np.interp(self.last_st_output, [0, 1], self.st_scales['driver_torque']) * 5
+    model_output = np.interp(self.last_st_output, [0, 1], self.st_scales['driver_torque']) * 3.5
 
     return int(round(model_output))
 
