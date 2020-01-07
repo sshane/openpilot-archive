852a9804027807d54a2bc466b7929a84a991ddde
Shane <shane@smiskol.com>
2020-01-07 10:33:17 -0600

    test

--- selfdrive/smart_torque/test.py
+++ selfdrive/smart_torque/test.py
@@ -5,7 +5,7 @@ st_model = st_wrapper.get_wrapper()
 st_model.init_model()
 start = time.time()
 for i in range(100):
-  model_output = st_model.run_model([.5]*202)
+  model_output = st_model.run_model([.5]*103)
 
 print('Took: {} seconds'.format(time.time() - start))
 print(model_output)
