26198db34d2146554a4b007983b992fd6343c744
Shane <shane@smiskol.com>
2020-01-18 13:53:06 -0600

    add my sentry key

--- selfdrive/crash.py
+++ selfdrive/crash.py
@@ -19,7 +19,7 @@ if os.getenv("NOLOG") or os.getenv("NOCRASH"):
 else:
   from raven import Client
   from raven.transport.http import HTTPTransport
-  client = Client('https://1994756b5e6f41cf939a4c65de45f4f2:cefebaf3a8aa40d182609785f7189bd7@app.getsentry.com/77924',
+  client = Client('https://aedc6d18281846bab2cf02f8f25f09a0:51c40ea2aa01407c809c05465152c672@sentry.io/1863770',
                   install_sys_hook=False, transport=HTTPTransport, release=version, tags={'dirty': dirty})
 
   def capture_exception(*args, **kwargs):
