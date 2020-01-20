9734ab6126d27b8b5c00619ce47156542c9cfc01
Shane <shane@smiskol.com>
2020-01-20 13:01:41 -0600

    average kf of corolla and tsp2 corolla

--- selfdrive/controls/lane_hugging.py
+++ selfdrive/controls/lane_hugging.py
@@ -14,13 +14,13 @@ class LaneHugging:
       self.direction = self.direction.lower()
     self.angle_offset = abs(self.op_params.get('lane_hug_angle_offset', 0.0))
 
-  def modify_offset(self, angle_offset_average, lane_change_direction, lane_change_state):
+  def modify_offset(self, offset, lane_change_direction, lane_change_state):
     # negative angles: right
     # positive angles: left
     if not travis:
       starting = LaneChangeState.laneChangeStarting
       if self.direction == 'left' and ((lane_change_state == starting and lane_change_direction != LaneChangeDirection.left) or lane_change_state != starting):
-        angle_offset_average = -self.angle_offset  # todo: not totally sure if this needs to be negative or positive lolol, will test and update
+        offset = -self.angle_offset  # todo: not totally sure if this needs to be negative or positive lolol, will test and update
       elif self.direction == 'right' and ((lane_change_state == starting and lane_change_direction != LaneChangeDirection.right) or lane_change_state != starting):
-        angle_offset_average = self.angle_offset
-    return angle_offset_average
+        offset = self.angle_offset
+    return offset
