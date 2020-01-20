f09deb115416e30597fdbd8768cc7a0595cc9c38
Shane <shane@smiskol.com>
2020-01-20 12:37:43 -0600

    use stock tuning with pedal test

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -64,8 +64,10 @@ class CarInterface(CarInterfaceBase):
     if ret.enableGasInterceptor:
       ret.gasMaxBP = [0., 9., 35]
       ret.gasMaxV = [0.2, 0.5, 0.7]
-      ret.longitudinalTuning.kpV = [1.2, 0.8, 0.5]
-      ret.longitudinalTuning.kiV = [0.18, 0.12]
+      # ret.longitudinalTuning.kpV = [1.2, 0.8, 0.5]
+      # ret.longitudinalTuning.kiV = [0.18, 0.12]
+      ret.longitudinalTuning.kpV = [3.6, 2.4, 1.5]
+      ret.longitudinalTuning.kiV = [0.54, 0.36]
     else:
       ret.gasMaxBP = [0.]
       ret.gasMaxV = [0.5]
