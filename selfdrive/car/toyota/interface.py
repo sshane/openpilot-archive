31ab70b046093ae3f966aefc2c74322637afa337
Shane Smiskol <shane@smiskol.com>
2020-02-01 22:18:31 -0600

    Test higher steering ratio

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -128,7 +128,7 @@ class CarInterface(CarInterfaceBase):
       stop_and_go = False
       ret.safetyParam = 100
       ret.wheelbase = 2.70
-      ret.steerRatio = 18.26 * 0.9
+      ret.steerRatio = 18.75
       tire_stiffness_factor = 0.444  # not optimized yet
       ret.mass = 2860. * CV.LB_TO_KG + STD_CARGO_KG  # mean between normal and hybrid
       # ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.2], [0.05]]
