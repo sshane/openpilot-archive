43769f2f02a1b9ff9f3fe7c9d5feee5c4c50b28b
Shane <shane@smiskol.com>
2020-02-09 04:38:06 -0600

    by bad, c

--- selfdrive/traffic/traffic.cc
+++ selfdrive/traffic/traffic.cc
@@ -108,7 +108,7 @@ extern "C" {
         initializeSNPE(runt);
     }
 
-    void multi_test(double inputArray[4][3][2], int x, int y, int z){
+    void multi_test(double inputArray[437][582][3], int x, int y, int z){
 //        std::cout << x;
 //        std::cout << "\n";
 //        std::cout << y;
