12750c8a9dbbb980e99ef05510b323ceaea5e617
Shane <shane@smiskol.com>
2020-02-10 00:29:10 -0600

    test

--- selfdrive/traffic/tf_test.py
+++ selfdrive/traffic/tf_test.py
@@ -19,5 +19,12 @@ interpreter.allocate_tensors()
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 
-print(input_details)
-print(output_details)
+input_shape = input_details[0]['shape']
+print('Model input shape: {}'.format(input_shape))
+# input_data = np.array(np.random.random_sample(input_shape), dtype=np.float32)
+interpreter.set_tensor(input_details[0]['index'], img)
+
+interpreter.invoke()
+
+output_data = interpreter.get_tensor(output_details[0]['index'])
+print(output_data)
