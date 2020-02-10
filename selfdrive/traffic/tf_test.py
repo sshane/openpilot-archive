6d4af628a435a1b65f0a99d8c86a9ab6a16d3413
Shane <shane@smiskol.com>
2020-02-10 01:01:39 -0600

    test

--- selfdrive/traffic/tf_test.py
+++ selfdrive/traffic/tf_test.py
@@ -12,28 +12,31 @@ W, H = 1164, 874
 # img = cv2.resize(img, dsize=(W // 2, H // 2), interpolation=cv2.INTER_CUBIC)
 # img = np.asarray(img, dtype=np.float32) / 255.  # normalize
 with open('/data/openpilot/selfdrive/traffic/phot_none_9989287', 'rb') as f:
-    img = np.array([pickle.load(f)])
-print(img)
-print(img.shape)
+    img1 = np.array([pickle.load(f)]).astype('float32')
 
-print(img.dtype)
+with open('/data/openpilot/selfdrive/traffic/phot_red_8513947', 'rb') as f:
+    img2 = np.array([pickle.load(f)]).astype('float32')
 
 interpreter = tflite.Interpreter(model_path='newest.tflite')
 # print(dir(interpreter))
 interpreter.allocate_tensors()
 
-t = time.time()
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
-print(input_details)
+# print(input_details)
 
 input_shape = input_details[0]['shape']
 print('Model input shape: {}'.format(input_shape))
 # input_data = np.array(np.random.random_sample(input_shape), dtype=np.float32)
-interpreter.set_tensor(input_details[0]['index'], img.astype('float32'))
+def predict(input_data):
+    interpreter.set_tensor(input_details[0]['index'], input_data)
+    interpreter.invoke()
+    output_data = interpreter.get_tensor(output_details[0]['index'])
+    return output_data
 
-interpreter.invoke()
+a = []
+t = time.time()
+for i in [img1, img2]:
+    a.append(predict(i))
 
-output_data = interpreter.get_tensor(output_details[0]['index'])
-print(time.time()-t)
-print(output_data)
+print(time.time() - t)
