9e07a585f1ebdc1212d8ab6a5cc2a739d6421b9d
Shane <shane@smiskol.com>
2020-02-10 00:30:05 -0600

    remove dir

--- selfdrive/traffic/tf_test.py
+++ selfdrive/traffic/tf_test.py
@@ -8,8 +8,8 @@ W, H = 1164, 874
 
 img = cv2.imread('/data/openpilot/selfdrive/traffic/GREEN_high.png')
 img = cv2.resize(img, dsize=(W // 2, H // 2), interpolation=cv2.INTER_CUBIC)
-
-
+img = np.asarray(img, dtype=np.float32)
+print(img)
 print(img.shape)
 
 interpreter = tflite.Interpreter(model_path='model.tflite')
