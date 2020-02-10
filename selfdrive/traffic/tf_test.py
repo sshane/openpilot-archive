e2afcea32d1f34f71daa0189cbd6941266bc35a0
Shane <shane@smiskol.com>
2020-02-10 00:34:23 -0600

    test time

--- selfdrive/traffic/tf_test.py
+++ selfdrive/traffic/tf_test.py
@@ -12,7 +12,7 @@ W, H = 1164, 874
 # img = cv2.resize(img, dsize=(W // 2, H // 2), interpolation=cv2.INTER_CUBIC)
 # img = np.asarray(img, dtype=np.float32) / 255.  # normalize
 with open('/data/openpilot/selfdrive/traffic/phot_red', 'rb') as f:
-    img = pickle.load(f)
+    img = np.array([pickle.load(f)])
 print(img)
 print(img.shape)
 
@@ -27,7 +27,7 @@ output_details = interpreter.get_output_details()
 input_shape = input_details[0]['shape']
 print('Model input shape: {}'.format(input_shape))
 # input_data = np.array(np.random.random_sample(input_shape), dtype=np.float32)
-interpreter.set_tensor(input_details[0]['index'], np.array([img]))
+interpreter.set_tensor(input_details[0]['index'], img)
 
 interpreter.invoke()
 
