a20ac6ef3ccfa87fddf4287d25f033f2f5a1a721
Shane <shane@smiskol.com>
2020-02-10 00:27:05 -0600

    test

---
+++ selfdrive/traffic/tf_test.py
@@ -0,0 +1,16 @@
+import tflite_runtime.interpreter as tflite
+import cv2
+import numpy as np
+import os
+
+os.chdir('/data/openpilot/selfdrive/traffic')
+W, H = 1164, 874
+
+img = cv2.imread('/data/openpilot/selfdrive/traffic/GREEN_high.png')
+img = cv2.resize(img, dsize=(W // 2, H // 2), interpolation=cv2.INTER_CUBIC)
+
+
+print(img.shape)
+
+interpreter = tflite.Interpreter(model_path='model.tflite')
+print(dir(interpreter))
