87dabea8c4853d3ae69c20cd605a633fc3ec21b7
Shane <shane@smiskol.com>
2020-02-14 10:35:03 -0600

    add a small buffer so we can gather images faster

--- selfdrive/traffic/gather_traffic.py
+++ selfdrive/traffic/gather_traffic.py
@@ -2,25 +2,42 @@ import cereal.messaging as messaging
 import numpy as np
 from PIL import Image
 import sys
+from threading import Thread
 import time
 
-def main():
+thread_count = 0
+last_msg = None
+
+
+def write_frame(msg_data):
+  global thread_count
+  thread_count += 1
+  image_data = msg_data.thumbnail.thumbnail
+  bgr_image_array = np.frombuffer(image_data[:(3840*874)], dtype=np.uint8).reshape((874,1280,3))
+  rgb_image_array = bgr_image_array[...,[2,1,0]]
+  # discard nulls
+  rgb_image_array = rgb_image_array[:,:1164]
+  rgb_image_array = rgb_image_array.reshape((874,1164,3))
+  img = Image.fromarray(rgb_image_array, 'RGB')
+  filename = time.strftime('%C%y%m%d%H%M%S') + '.png'
+  img.save('/data/openpilot/selfdrive/traffic/imgs/{}'.format(filename))
+  thread_count -= 1
+
+
+def gather_loop():
+  global thread_count
+  global last_msg
   time.sleep(10)  # give time for everything to start up
   image_sock = messaging.sub_sock('image')
   while True:
-    print("Reading image from socket.")
     msg_data = messaging.recv_one(image_sock)
-    print("Image received, transforming.")
-    image_data = msg_data.thumbnail.thumbnail
-    bgr_image_array = np.frombuffer(image_data[:(3840*874)], dtype=np.uint8).reshape((874,1280,3))
-    rgb_image_array = bgr_image_array[...,[2,1,0]]
-    # discard nulls
-    rgb_image_array = rgb_image_array[:,:1164]
-    rgb_image_array = rgb_image_array.reshape((874,1164,3))
-    img = Image.fromarray(rgb_image_array, 'RGB')
-    filename = time.strftime('%C%y%m%d%H%M%S') + '.png'
-    print("Saving: " + filename)
-    img.save('/data/openpilot/selfdrive/traffic/imgs/{}'.format(filename))
+    if msg_data != last_msg:
+      last_msg = msg_data
+      while thread_count > 20:  # gives us a buffer of 20 frames
+        time.sleep(0.5)
+
+      Thread(target=write_frame, args=(msg_data,)).start()
+
 
 if __name__ == '__main__':
-  main()
+  gather_loop()
