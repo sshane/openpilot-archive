e92c27f6cf97879b18b58d807bd82aa8f1db81b8
Shane <shane@smiskol.com>
2020-02-14 11:00:45 -0600

    test

--- selfdrive/traffic/test.py
+++ selfdrive/traffic/test.py
@@ -9,7 +9,7 @@ import cv2
 W, H = 1164, 874
 y_hood_crop = 665
 traffic_model, ffi = traffic_wrapper.get_wrapper()
-traffic_model.init_model()
+# traffic_model.init_model()
 data_dir = '/data/openpilot/selfdrive/traffic/test_images'
 
 image = cv2.imread('{}/GREEN/{}'.format(data_dir, '20200210202741.0.png')).astype(np.float32)
