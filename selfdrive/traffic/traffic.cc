fe630f8651ead04391f2164f21925182089861cc
Shane <shane@smiskol.com>
2020-02-14 12:19:20 -0600

    test

--- selfdrive/traffic/traffic.cc
+++ selfdrive/traffic/traffic.cc
@@ -81,7 +81,7 @@ std::unique_ptr<zdl::DlSystem::ITensor> loadInputTensorNew(std::unique_ptr<zdl::
     std::cout << "here1\n";
     /* Copy the loaded input file contents into the networks input tensor.SNPE's ITensor supports C++ STL functions like std::copy() */
 
-    std::copy(inputVec.begin(), inputVec.begin() + 500000, input->begin());
+    std::copy(inputVec.begin(), inputVec.end(), std::back_inserter(input->begin()));
     std::cout << "here\n";
     return input;
 
