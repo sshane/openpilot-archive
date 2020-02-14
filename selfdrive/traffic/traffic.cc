bd80596f0ceb4ec7989e02b9a7edcd0d122c51dd
Shane <shane@smiskol.com>
2020-02-14 11:50:38 -0600

    test

--- selfdrive/traffic/traffic.cc
+++ selfdrive/traffic/traffic.cc
@@ -150,7 +150,7 @@ extern "C" {
         returnOutputMulti(oTensor);
     }
 
-    int predict_traffic(int inputArray[2322180]){
+    int predict_traffic(float inputArray[2322180]){
         std::cout << inputArray[0] << "\n";
         std::cout << inputArray[1] << "\n";
         std::cout << inputArray[2] << "\n";
