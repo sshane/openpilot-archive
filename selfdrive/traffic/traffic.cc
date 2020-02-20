0c71c084fb39c71c0ea0a43a426531601622a39b
Shane <shane@smiskol.com>
2020-02-20 14:30:33 -0600

    test third loop

--- selfdrive/traffic/traffic.cc
+++ selfdrive/traffic/traffic.cc
@@ -166,7 +166,7 @@ int visionTest(){
     double proc_start = millis_since_boot();
     for(int line=0;line<515;line++) {
         for(int line_pos = 0; line_pos < (814 * 3); line_pos += 3) {
-            for(int val=0, val < 3; val++){
+            for(int val=0; val < 3; val++){
                 modelInput.push_back(src_ptr[line_pos + offset + val] / 255.0);
             }
 //            dst_ptr[line_pos + 0] = src_ptr[line_pos + offset + 2];
