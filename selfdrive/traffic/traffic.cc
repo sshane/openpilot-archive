5db463e4c2875656e10fa40d07683167f199db8b
Shane <shane@smiskol.com>
2020-02-20 17:38:46 -0600

    test sleep for 5 sec

--- selfdrive/traffic/traffic.cc
+++ selfdrive/traffic/traffic.cc
@@ -193,7 +193,7 @@ extern "C" {
 
             loopEnd = millis_since_boot();
 //            std::cout << "Loop time: " << (loopEnd - loopStart) * msToSec << " sec\n";
-            usleep(1 * secToUs);
+            usleep(5 * secToUs);
 
             if (shouldStop()){
                 break;
