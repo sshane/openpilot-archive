0656f9dcd41da69906b9d8bc2ed7e4ee31f9a2dc
Shane <shane@smiskol.com>
2020-02-19 21:34:18 -0600

    test

--- selfdrive/traffic/traffic.cc
+++ selfdrive/traffic/traffic.cc
@@ -166,8 +166,8 @@ int visionTest(){
     }
     printf("%i\n", ((uint8_t (*)[814][515]) img)[0][0][0]);
 
-    for (int x = 0; x <= 814; x++){
-        for (int y = 0; y <= 515; y++){
+    for (int x = 0; x <= 515; x++){
+        for (int y = 0; y <= 814; y++){
             if (idx < 100){
                 std::cout << (int)((uint8_t (*)[814][515]) img)[x][y][0] << " " << (int)((uint8_t (*)[814][515]) img)[x][y][1] << " " << (int)((uint8_t (*)[814][515]) img)[x][y][2] << std::endl;
                 printf("%i\n", ((uint8_t (*)[814][515]) img)[x][y][0]);
