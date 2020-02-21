bb85c78d7e9800e1c3109f544cf0f33d1e2025e4
Shane <shane@smiskol.com>
2020-02-20 18:39:36 -0600

    comment out prediction

--- selfdrive/traffic/traffic.cc
+++ selfdrive/traffic/traffic.cc
@@ -211,7 +211,7 @@ extern "C" {
             std::vector<float> modelOutput = runModel(inputVector);
 
             int pred_idx = std::max_element(modelOutput.begin(), modelOutput.end()) - modelOutput.begin();
-            std::cout << "Prediction: " << modelLabels[pred_idx] << " (" << modelOutput[pred_idx] * 100 << "%)" << std::endl;
+            //std::cout << "Prediction: " << modelLabels[pred_idx] << " (" << modelOutput[pred_idx] * 100 << "%)" << std::endl;
 
             // sleepFor(0.5);  // in seconds
             loopEnd = millis_since_boot();
