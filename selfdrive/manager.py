e8234ec5f4c81765729eb7896f316792effe8b43
Shane <shane@smiskol.com>
2020-02-22 00:47:54 -0600

    revert

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -80,7 +80,7 @@ if not prebuilt:
 
     nproc = os.cpu_count()
     j_flag = "" if nproc is None else "-j%d" % (nproc - 1)
-    scons = subprocess.Popen(["scons", j_flag, '--config=force'], cwd=BASEDIR, env=env, stderr=subprocess.PIPE)
+    scons = subprocess.Popen(["scons", j_flag], cwd=BASEDIR, env=env, stderr=subprocess.PIPE)
 
     # Read progress from stderr and update spinner
     while scons.poll() is None:
