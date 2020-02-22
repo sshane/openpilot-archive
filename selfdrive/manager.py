0b43c563549d8aa5e1b7bc4815bd2f85292f8ff2
Shane <shane@smiskol.com>
2020-02-22 02:56:58 -0600

    add option to run all

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -429,7 +429,9 @@ def manager_thread():
     if msg.thermal.freeSpace < 0.05:
       logger_dead = True
 
-    if msg.thermal.started:
+    run_all = True
+
+    if msg.thermal.started or run_all:
       for p in car_started_processes:
         if p == "loggerd" and logger_dead:
           kill_managed_process(p)
