145dfac7982966bfeada65990c1c2481b3e6e3ea
Shane <shane@smiskol.com>
2020-02-23 05:43:43 -0600

    test

--- selfdrive/trafficd/trafficd.cc
+++ selfdrive/trafficd/trafficd.cc
@@ -272,8 +272,8 @@ int main(){
             }
             std::vector<float> imageVector = getFlatVector(buf, true);  // writes float vector to inputVector
 
-             ofstream outputfile("/data/cropped");
-             std::copy(outputVector.rbegin(), outputVector.rend(), std::ostream_iterator<float>(outputfile, "\n"));
+            ofstream outputfile("/data/cropped");
+            std::copy(outputVector.rbegin(), outputVector.rend(), std::ostream_iterator<float>(outputfile, "\n"));
 
 //            std::cout << "Vector size: " << imageVector.size() << std::endl;
 
