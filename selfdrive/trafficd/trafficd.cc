974696dbaafa5dcc5a98c047fae54c4deb624eb5
Shane <shane@smiskol.com>
2020-02-23 02:16:17 -0600

    test crop

--- selfdrive/trafficd/trafficd.cc
+++ selfdrive/trafficd/trafficd.cc
@@ -193,10 +193,7 @@ static std::vector<uint8_t> yuv420p_to_rgb2(const uint8_t* y, const uint8_t* u, 
     uint8_t* src_ptr = rgb;
     std::vector<uint8_t> img;
     int y_test = 0;
-    for (int y_cord = 0; y_cord < height; y_cord++) {
-        if (y_cord <= 150 | y_cord >= 665){
-            continue;
-        }
+    for (int y_cord = 150; y_cord < 665+150; y_cord++) {
         y_test++;
         int x_test = 0;
         for (int x_cord = 0; x_cord < width; x_cord++) {
