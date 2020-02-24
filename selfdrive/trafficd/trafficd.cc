3db72606aa832e688eb2ccccca34b6cddc414503
Shane <shane@smiskol.com>
2020-02-23 22:06:57 -0600

    all done

--- selfdrive/trafficd/trafficd.cc
+++ selfdrive/trafficd/trafficd.cc
@@ -286,7 +286,7 @@ int main(){
             sendPrediction(modelOutputVec, traffic_lights_sock);
 
             loopEnd = millis_since_boot();
-            std::cout << "Loop time: " << loopEnd - loopStart << " ms\n";
+            // std::cout << "Loop time: " << loopEnd - loopStart << " ms\n";
             lastLoop = rateKeeper(loopEnd - loopStart, lastLoop);
             // std::cout << "Current frequency: " << 1 / ((millis_since_boot() - loopStart) * msToSec) << " Hz" << std::endl;
         }
