0df324c3e8c44a05356c3142ef94cd49320653fa
Shane <shane@smiskol.com>
2020-02-26 17:28:29 -0600

    increase model rate

--- selfdrive/trafficd/traffic_manager.py
+++ selfdrive/trafficd/traffic_manager.py
@@ -15,7 +15,7 @@ class Traffic:
     self.labels = ['RED', 'GREEN', 'YELLOW', 'NONE']
 
     self.past_preds = []
-    self.model_rate = 1 / 3.
+    self.model_rate = 1 / 4.
     self.recurrent_length = 1.0  # in seconds, how far back to factor into current prediction
     self.des_pred_len = int(round(self.recurrent_length / self.model_rate))
     self.last_pred_weight = 8.  # places 2x weight on most recent prediction
