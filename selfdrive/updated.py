e096e8edac69b7db37c36f96a7fce0bcfca86ca8
Shane Smiskol <shane@smiskol.com>
2020-03-09 07:18:54 -0500

    update

--- selfdrive/updated.py
+++ selfdrive/updated.py
@@ -325,7 +325,7 @@ def main(gctx=None):
   except IOError:
     raise RuntimeError("couldn't get overlay lock; is another updated running?")
 
-  time_offroad = time.time()
+  time_offroad = time.time() - (3 * 60)
   need_reboot = False
   while True:
     time_wrong = datetime.datetime.now().year < 2019
