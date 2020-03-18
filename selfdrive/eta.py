6a1eaf4f4bfd6272154b4342652553c0fd0dd10a
Shane Smiskol <shane@smiskol.com>
2020-03-18 13:39:58 -0500

    refactor up eta

--- selfdrive/eta.py
+++ selfdrive/eta.py
@@ -2,48 +2,35 @@ import datetime
 
 
 class ETA:
-    def __init__(self, start_time, max_progress):  # only supports up to minutes
-        self.start_time = start_time
-        self.max_progress = max_progress
-        self.progress = 0
-        self.time = 0
-        self.etr = 0  # in seconds, estimated time remained
-        self.seconds = 60
+  def __init__(self, start_time, max_progress):  # only supports up to minutes
+    self.start_time = start_time
+    self.max_progress = max_progress
+    self.progress = 0
+    self.time = 0
+    self.etr = 0  # in seconds, estimated time remained
+    self.seconds = 60
 
-    # def init(self, t, max_progress):
-    #     self.start_time = t
-    #     self.max_progress = max_progress
+  # def init(self, t, max_progress):
+  #     self.start_time = t
+  #     self.max_progress = max_progress
 
-    def log(self, progress, t):
-        self.progress = progress
-        self.time = t
+  def log(self, progress, t):
+    self.progress = progress
+    self.time = t
 
-    def get_eta(self):
-        elapsed = self.time - self.start_time
-        etr = self.max_progress * (elapsed / (self.progress + 1)) - elapsed
-        print(etr)
-        print(self.progress)
-        print(self.time)
+  def get_eta(self):
+    elapsed = self.time - self.start_time
+    etr = self.max_progress * (elapsed / (self.progress + 1)) - elapsed
+    hours, remainder = divmod(round(etr), self.seconds**2)
+    minutes, seconds = divmod(remainder, self.seconds)
 
-        sec_string = ''
-        min_string = ''
-        etr = datetime.timedelta(seconds=round(etr))
-        etr = str(etr).split(':')[-2:]
-        minutes = int(etr[0])
-        seconds = int(etr[1])
+    time_list = [hours, minutes, seconds]
+    time_str_list = ['hour', 'minute', 'second']
 
-        sec_string += '{} second'.format(seconds)
-        min_string += '{} minute'.format(minutes)
+    etr_list = []
+    for t, t_str in zip(time_list, time_str_list):
+      plural = 's' if t != 1 else ''
+      if t != 0:
+        etr_list.append('{} {}{}'.format(hours, t_str, plural))
 
-        if seconds != 1:
-            sec_string += 's'
-        if minutes != 1:
-            min_string += 's'
-
-        etr_string = []
-        if minutes != 0:
-            etr_string.append(min_string)
-        etr_string.append(sec_string)
-        etr_string = ', '.join(etr_string)
-
-        return etr_string
+    return ', '.join(etr_list)
