c79b25e469036b5c2912b99da86b07300d5f4e5d
Shane Smiskol <shane@smiskol.com>
2020-03-18 13:19:19 -0500

    test

--- selfdrive/common/spinner.c
+++ selfdrive/common/spinner.c
@@ -84,7 +84,8 @@ int spin(int argc, char** argv) {
     // Check stdin for new text
     if (stdin_input_available()){
       fgets(spintext, SPINTEXT_LENGTH, stdin);
-      spintext[strcspn(spintext, "\n")] = 0;
+      size_t len = strlen(spintext);
+      spintext[len] = 0;
 
       // Get current status
       has_extra = strchr(spintext, ',') != NULL;
@@ -94,7 +95,6 @@ int spin(int argc, char** argv) {
       }
 
       // Check if number (update progress bar)
-      size_t len = strlen(spintext);
       bool is_number = len > 0;
       for (int i = 0; i < len; i++){
         if (!isdigit(spintext[i])){
