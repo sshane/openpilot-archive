3ed311f76c2aa341fc504631fec74ac0bcd3b888
Shane Smiskol <shane@smiskol.com>
2020-03-18 18:07:08 -0500

    test

--- selfdrive/eta.py
+++ selfdrive/eta.py
@@ -51,14 +51,14 @@ class ETA:
     if last_ips < total_ips:
       ips = last_ips * 0.8 + total_ips * 0.8
 
-    remaining = self.max_progress - self.progress
-    return 'compiling: {}% ETA: {}'.format(percentage, self.format_etr(remaining / ips))
-
     if last_ips > 10:  # probably pulling from cache
       # self.start_time = time.time()  # ensures ips accuracy
-      # self.progress_subtract = self.progress
+      self.progress_subtract = self.progress
       remaining = self.max_progress - self.progress
       return 'checking compiled: {}% ETA: {}'.format(percentage, self.format_etr(remaining / ips))
+
+    remaining = self.max_progress - self.progress
+    return 'compiling: {}% ETA: {}'.format(percentage, self.format_etr(remaining / ips))
 
     times_idx = len(self.times) * (self.progress / self.max_progress)
     if times_idx == round(times_idx):
