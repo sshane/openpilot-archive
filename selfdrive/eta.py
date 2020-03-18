53761338d0885a4d71ec46353721523000dc77b0
Shane Smiskol <shane@smiskol.com>
2020-03-18 14:23:09 -0500

    test

--- selfdrive/eta.py
+++ selfdrive/eta.py
@@ -25,8 +25,9 @@ class ETA:
   def get_eta(self):
     elapsed = self.time - self.start_time
     last_elapsed = (self.time - self.last_time) / (self.progress - self.last_progress)
-    print(last_elapsed)
-    print(self.progress - self.last_progress)
+    # print(last_elapsed)
+    # print(self.progress - self.last_progress)
+    print(self.last_progress)
 
     percentage = elapsed / (self.progress + 1)
     factor = last_elapsed - (elapsed / self.progress)
