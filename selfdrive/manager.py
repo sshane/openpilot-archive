e010b9568b736e73488cdf71d7dcd6e1528e6297
Shane Smiskol <shane@smiskol.com>
2020-03-18 17:02:29 -0500

    test

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -99,6 +99,7 @@ if not prebuilt:
     ips = 0
     total_ips = 0
     last_eta_time = time.time()
+    start_time = time.time()
     while scons.poll() is None:
       # try:
       line = scons.stderr.readline()
@@ -110,14 +111,15 @@ if not prebuilt:
       if line.startswith(prefix):
         i = int(line[len(prefix):])
         if spinner is not None:
-          eta_tool.log(i, time.time())
-          if (time.time() - last_eta_time) > 1:
-            last_eta, ips, total_ips = eta_tool.get_eta()
-            last_eta_time = time.time()
-
+          # eta_tool.log(i, time.time())
+          # if (time.time() - last_eta_time) > 1:
+          #   last_eta, ips, total_ips = eta_tool.get_eta()
+          #   last_eta_time = time.time()
+          with open('/data/scons_times', 'a') as f:
+            f.write('{}\n'.format(time.time() - start_time))
           percentage = i / TOTAL_SCONS_NODES
-          # spinner.update("%d" % (percentage * scons_finished_progress), 'compiling: {}% (ETA: {}, IPS: {})'.format(round(percentage * 100, 1), last_eta, ips))
-          spinner.update("%d" % (percentage * scons_finished_progress), 'ETA: {}, last IPS: {}, total IPS: {}'.format(last_eta, ips, total_ips))
+          spinner.update("%d" % (percentage * scons_finished_progress), 'storing times: {}%'.format(round(percentage * 100, 1)))
+          # spinner.update("%d" % (percentage * scons_finished_progress), 'ETA: {}, last IPS: {}, total IPS: {}'.format(last_eta, ips, total_ips))
       elif len(line):
         print(line.decode('utf8'))
       # except Exception:
