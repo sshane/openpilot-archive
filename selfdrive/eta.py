cfa8bb4302f5dcd32912434fe8266d731cc1b256
Shane Smiskol <shane@smiskol.com>
2020-03-18 21:15:42 -0500

    test

--- selfdrive/eta.py
+++ selfdrive/eta.py
@@ -87,7 +87,7 @@ class ETA(threading.Thread):
     if self.this_ips < 10 < self.last_ips and self.updated:
       print('RESET HERE!!!\n---------')
       self.start_time = float(self.get_eta_data(-1).time)  # reset total ips when we stop getting cached files
-      self.progress_subtract = int(self.get_eta_data(-1).progress)
+      self.progress_subtract = int(self.get_eta_data(-2).progress)
 
     self.total_ips = (self.get_eta_data().progress - self.progress_subtract) / (cur_time - self.start_time)
 
@@ -103,16 +103,16 @@ class ETA(threading.Thread):
       if self.last_ips < ips:
         ips = self.last_ips * 0.8 + ips * 0.2
 
-    # if self.this_ips > 10:  # probably pulling from cache
-    #   remaining = self.max_progress - self.get_eta_data().progress
-    #   return 'compiled: {}% ETA: {}'.format(percentage, self.format_etr(remaining / ips))
+    if self.this_ips > 10:  # probably pulling from cache
+      remaining = self.max_progress - self.get_eta_data().progress
+      return 'compiled: {}% ETA: {}'.format(percentage, self.format_etr(remaining / ips))
 
     etr = self.format_etr((self.max_progress - self.get_eta_data().progress) / ips)
-    print("ETA: {}".format(etr))
+    # print("ETA: {}".format(etr))
 
-    return 'TOTAL IPS: {} - CUR IPS: {} - LAST IPS: {} - USING IPS: {}'.format(round(self.total_ips, 2), round(self.this_ips, 2), round(self.last_ips, 2), round(ips, 2))
+    # return 'TOTAL IPS: {} - CUR IPS: {} - LAST IPS: {} - USING IPS: {}'.format(round(self.total_ips, 2), round(self.this_ips, 2), round(self.last_ips, 2), round(ips, 2))
 
-    # return 'compiling: {}% ETA: {}'.format(percentage, etr)
+    return 'compiling: {}% ETA: {}'.format(percentage, etr)
 
 
 
