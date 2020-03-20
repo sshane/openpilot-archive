13d1f2b38cfd515f3e158c1dcc1ca2f8b21c8ae0
Shane <shane@smiskol.com>
2020-03-20 07:15:30 -0500

    this should do it

--- selfdrive/eta.py
+++ selfdrive/eta.py
@@ -90,7 +90,7 @@ class ETA(threading.Thread):
     if self.this_ips < 10 < self.last_ips:  # and self.updated:
       print('RESET HERE!!!\n---------')
       self.start_time = cur_time  # reset total ips when we stop getting cached files
-      self.progress_subtract = int(self.get_eta_data(-2).progress)
+      self.progress_subtract = int(self.get_eta_data(-1).progress)
       self.total_ips = self.this_ips
     else:
       self.total_ips = (self.get_eta_data().progress - self.progress_subtract) / (cur_time - self.start_time)
