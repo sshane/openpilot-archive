93f4fe856239b1d01bdfdcac184c7cdbfa0073c2
Shane <shane@smiskol.com>
2020-03-24 12:24:15 -0500

    fix

--- selfdrive/common/spinner.c
+++ selfdrive/common/spinner.c
@@ -187,7 +187,7 @@ int spin(int argc, char** argv) {
       nvgFontSize(vg, 59.0f);
       // nvgTextBox(s->vg, metric_x + 35, metric_y + (strlen(message_str) > 8 ? 40 : 50), metric_w - 50, message_str, NULL);
       int new_line_length = 1600;
-      nvgTextBox(vg, fb_w/2 - (new_line_length / 2), (fb_h*2/3)+24+96, new_line_length, spinstatus, NULL);
+      nvgTextBox(vg, fb_w/2 - (new_line_length / 2), (fb_h*2/3)+24+96, new_line_length, spinerror, NULL);
     }
 
     nvgEndFrame(vg);
