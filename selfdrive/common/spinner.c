ae5fbedd417855f8f608bd59140445617f1ce508
Shane <shane@smiskol.com>
2020-03-24 12:37:50 -0500

    better

--- selfdrive/common/spinner.c
+++ selfdrive/common/spinner.c
@@ -183,8 +183,9 @@ int spin(int argc, char** argv) {
       nvgFontSize(vg, 96.0f);
       nvgText(vg, fb_w/2, (fb_h*2/3)+24, spintext, NULL);
     } else if (has_extra) {  // error message
+      float fontsize = (strchr(spintext, "error: ") != NULL) ? 59.0f : 78.0f;
       nvgTextAlign(vg, NVG_ALIGN_CENTER | NVG_ALIGN_TOP);
-      nvgFontSize(vg, strchr(spintext, "error: ") != NULL ? 59.0f : 78.0f);
+      nvgFontSize(vg, 59.0f);
       // nvgTextBox(s->vg, metric_x + 35, metric_y + (strlen(message_str) > 8 ? 40 : 50), metric_w - 50, message_str, NULL);
       int new_line_length = 1600;
       nvgTextBox(vg, fb_w/2 - (new_line_length / 2), (fb_h*2/3)+24+96, new_line_length, spinstatus, NULL);
