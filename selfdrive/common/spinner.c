5790fba3bc3e1a7e1e46de0bac4a80b4db22070f
Shane <shane@smiskol.com>
2020-03-29 00:32:29 -0500

    revert

--- selfdrive/common/spinner.c
+++ selfdrive/common/spinner.c
@@ -189,7 +189,7 @@ int spin(int argc, char** argv) {
       nvgFontSize(vg, err_msg ? 59.0f : 78.0f);
       if (err_msg) {  // need smaller font for error msg
         int new_line_length = 1300;
-        int y_offset = strlen(spinstatus) > 180 ? 20 : 96;
+        int y_offset = strlen(spinstatus) > 180 ? 76 : 96;
         nvgTextBox(vg, (fb_w/2)-(new_line_length/2), (fb_h*2/3)+24+y_offset, new_line_length, spinstatus, NULL);
       } else {
         nvgText(vg, fb_w/2, (fb_h*2/3)+24+96, spinstatus, NULL);
