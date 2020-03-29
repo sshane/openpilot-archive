6fe314e1bf54006f1c5d5ed908cef04a4996a993
Shane <shane@smiskol.com>
2020-03-29 00:09:43 -0500

    test

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -114,8 +114,8 @@ if not prebuilt:
             # str_err = re.search('error: (.*)\n', line).span()
             error_txt = 'error: {} end'
             line = error_txt.format(''.join([random.choice(string.ascii_lowercase + '           ') for i in range(530 + 2 - len(error_txt))]))
-            if len(line) > 185:
-              line = line[:185].strip() + '...'
+            if len(line) > 186:
+              line = line[:186].strip() + '...'
             spinner.update("%d" % (scons_finished_progress * (p / TOTAL_SCONS_NODES)), line)
             time.sleep(60*60)
             break
