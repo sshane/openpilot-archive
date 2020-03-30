f5bf7c5a93311305bd0285e6a33f524550c6d322
Shane <shane@smiskol.com>
2020-03-30 12:01:28 -0500

    less integral

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -33,7 +33,7 @@ class CarInterface(CarInterfaceBase):
       ret.gasMaxBP = [0., 9., 35]
       ret.gasMaxV = [0.2, 0.5, 0.7]
       ret.longitudinalTuning.kpV = [1.2, 0.8, 0.5]
-      ret.longitudinalTuning.kiV = [0.18, 0.12]
+      ret.longitudinalTuning.kiV = [0.17, 0.108]
     else:
       ret.gasMaxBP = [0.]
       ret.gasMaxV = [0.5]
