735739170c43f9a7c153f2089c455b19e9139cdf
Shane <shane@smiskol.com>
2020-03-30 18:15:03 -0500

    fix

--- selfdrive/common/spinner.c
+++ selfdrive/common/spinner.c
@@ -196,7 +196,6 @@ int spin(int argc, char** argv) {
       nvgText(vg, fb_w/2, (fb_h*2/3)+24, spintext, NULL);
     } else if (has_extra) {
       nvgTextAlign(vg, NVG_ALIGN_CENTER | NVG_ALIGN_TOP);
-      err_msg = true;
       if (err_msg) {
         printf("IS ERR MSG!\n");
         int break_row_width = 1300;
@@ -207,8 +206,8 @@ int spin(int argc, char** argv) {
         fontsize = strlen(spinerr) > 120 ? 59.0f : fontsize;
 
         nvgFontSize(vg, fontsize);
-        // nvgTextBox(vg, (fb_w/2)-(break_row_width/2), (fb_h*2/3)+24+y_offset, break_row_width, "Test\nnewline!", NULL);
-        nvgText(vg, fb_w/2, (fb_h*2/3)+24+y_offset, spinerr, NULL);
+        nvgTextBox(vg, (fb_w/2)-(break_row_width/2), (fb_h*2/3)+24+y_offset, break_row_width, spinerr, NULL);
+        // nvgText(vg, fb_w/2, (fb_h*2/3)+24+y_offset, spinerr, NULL);
       } else {
         nvgFontSize(vg, 78.0f);
         nvgText(vg, fb_w/2, (fb_h*2/3)+24+96, spinstatus, NULL);
