70084c03adc349765b76c566e268e23c4095bc34
Shane <shane@smiskol.com>
2020-03-31 16:46:13 -0500

    debug

--- test_spin.py
+++ test_spin.py
@@ -3,9 +3,9 @@ from common.spinner import Spinner
 
 spinner = Spinner()
 
-for i in range(100):
-  spinner.update("30")
-time.sleep(0.17)
+
+spinner.update("30")
+# time.sleep(0.17)
 spinner.update("ERR,Test error")
 print('shown error!', flush=True)
 time.sleep(10)
