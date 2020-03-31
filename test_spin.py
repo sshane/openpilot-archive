4e21cfc10b67b8f2c6d94a4b358aa317c9590075
Shane <shane@smiskol.com>
2020-03-31 15:54:12 -0500

    test

---
+++ test_spin.py
@@ -0,0 +1,10 @@
+import time
+from common.spinner import Spinner
+
+spinner = Spinner()
+
+spinner.update("30")
+time.sleep(0.17)
+spinner.update("ERR,Test error")
+print('shown error!', flush=True)
+time.sleep(10)
