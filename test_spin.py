d2ecb812be9920a435e6138368869724db9c09a6
Shane <shane@smiskol.com>
2020-03-31 17:56:10 -0500

    test

--- test_spin.py
+++ test_spin.py
@@ -8,7 +8,7 @@ spinner.update("30")
 time.sleep(1)
 spinner.update("ERR,Test error")
 print('shown error!', flush=True)
-time.sleep(5)
-
-spinner.update("ERR,Test error2")
 time.sleep(10)
+
+# spinner.update("ERR,Test error2")
+# time.sleep(10)
