8822edc47706025953226d85a889b69aa319cd3a
Shane <shane@smiskol.com>
2020-03-31 15:58:11 -0500

    test

--- test_spin.py
+++ test_spin.py
@@ -3,8 +3,8 @@ from common.spinner import Spinner
 
 spinner = Spinner()
 
-spinner.update("30")
-time.sleep(0.5)
+spinner.update("10")
+# time.sleep(0.5)
 spinner.update("90")
 print('shown error!', flush=True)
 time.sleep(10)
