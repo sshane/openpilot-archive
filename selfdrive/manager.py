a73109e4e8fe648dab70b6953a8b2179327f675b
Shane <shane@smiskol.com>
2020-04-02 01:23:05 -0500

    run all

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -443,7 +443,7 @@ def manager_thread():
     if msg.thermal.freeSpace < 0.05:
       logger_dead = True
 
-    if msg.thermal.started:
+    if msg.thermal.started or True:
       for p in car_started_processes:
         if p == "loggerd" and logger_dead:
           kill_managed_process(p)
