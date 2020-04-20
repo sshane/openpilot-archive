23ce5a0322a6c00daeeaae711e47f5946f2f7f57
Shane <shane@smiskol.com>
2020-04-20 00:30:43 -0500

    use np

--- selfdrive/smart_torque/test.py
+++ selfdrive/smart_torque/test.py
@@ -6,7 +6,7 @@ st_model = st_wrapper.get_wrapper()
 st_model.init_model()
 start = time.time()
 samples = np.random.rand(100, 103)
-for i in range(samples):
+for i in samples:
   model_output = st_model.run_model(i)
 
 print('Took: {} seconds'.format(time.time() - start))
