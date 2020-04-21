e44d91223ab1a280267e69079d2be17fcd19b5c8
Shane <shane@smiskol.com>
2020-04-20 22:52:12 -0500

    test

--- selfdrive/smart_torque/benchmark.py
+++ selfdrive/smart_torque/benchmark.py
@@ -5,16 +5,16 @@ from selfdrive.smart_torque.dense_model import predict
 samples = np.random.rand(20000, 103).tolist()
 print('samples: {}\n'.format(len(samples)))
 
-t = time.time()
-predict(samples)
-t = time.time() - t
-print('Konverted model batch prediction time: {}s'.format(round(t, 6)))
+# t = time.time()
+# predict(samples)
+# t = time.time() - t
+# print('Konverted model batch prediction time: {}s'.format(round(t, 6)))
 
 print('-----')
 
-# t = time.time()
-# konverter_preds = []
-# for i in samples:
-#   predict(i)
-# t = time.time() - t
-# print('Konverted model single prediction time: {}s'.format(round(t, 6)))
+t = time.time()
+konverter_preds = []
+for i in samples:
+  predict(i)
+t = time.time() - t
+print('Konverted model single prediction time: {}s'.format(round(t, 6)))
