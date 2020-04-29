511a9c24fc5d283db1c27fce1ec0537e977c0dd6
Shane <shane@smiskol.com>
2020-04-28 20:06:06 -0500

    new full dense model

--- selfdrive/controls/lib/latcontrol_pid.py
+++ selfdrive/controls/lib/latcontrol_pid.py
@@ -32,8 +32,8 @@ class LatControlPID():
     self.gather_data = False
 
     self.last_pred_time = 0
-    self.x_length = 1.00  # seconds
-    self.y_length = 0.25
+    self.x_length = 1.  # seconds
+    self.y_length = 0.1
     # self.scales = {'delta_desired': [-58.75222851843965, 38.45948040492657],
     #                'rate_desired': [-0.07444784045219421, 0.032148655503988266], 'driver_torque': [-286.0, 277.0],
     #                'eps_torque': [-487.0, 471.0], 'angle_steers': [-47.70000076293945, 37.599998474121094],
