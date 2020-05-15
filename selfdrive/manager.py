591dccf385829b756f8efa9ae1725a9d9615df6c
Shane <shane@smiskol.com>
2020-05-14 20:27:16 -0500

    revert

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -477,7 +477,7 @@ def manager_thread():
     if msg.thermal.freeSpace < 0.05:
       logger_dead = True
 
-    if True:
+    if msg.thermal.started and "driverview" not in running:
       for p in car_started_processes:
         if p == "loggerd" and logger_dead:
           kill_managed_process(p)
