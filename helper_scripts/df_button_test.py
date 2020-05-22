c890b5ea437653807962d33f631d36ac3b90189f
Shane <shane@smiskol.com>
2020-05-21 21:37:15 -0500

    remove unneeded files

--- helper_scripts/df_button_test.py
+++
@@ -1,8 +0,0 @@
-import time
-import cereal.messaging as messaging
-
-sm = messaging.SubMaster(['dynamicFollowButton'])
-while True:
-  sm.update(0)
-  print(sm['dynamicFollowButton'].status)
-  time.sleep(1)
