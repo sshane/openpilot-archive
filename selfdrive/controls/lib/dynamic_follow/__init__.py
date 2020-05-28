fed4a87e00ccc09caa5b7e9109d3d3293cb7cb1f
Shane <shane@smiskol.com>
2020-05-28 00:49:26 -0500

    test integral-inspired df

--- selfdrive/controls/lib/dynamic_follow/__init__.py
+++ selfdrive/controls/lib/dynamic_follow/__init__.py
@@ -49,6 +49,7 @@ class DynamicFollow:
     self.data_collector = DataCollector(file_path='/data/df_data', keys=['v_ego', 'a_lead', 'v_lead', 'x_lead', 'live_tracks', 'profile', 'time'])
 
   def _setup_changing_variables(self):
+    self.TR_mod = 0
     self.TR = self.default_TR
     self.user_profile = self.df_profiles.relaxed  # just a starting point
     self.model_profile = self.df_profiles.relaxed
@@ -231,79 +232,31 @@ class DynamicFollow:
     return TR, profile_mod_pos, profile_mod_neg
 
   def _get_TR(self):
-    x_vel = [0.0, 1.8627, 3.7253, 5.588, 7.4507, 9.3133, 11.5598, 13.645, 22.352, 31.2928, 33.528, 35.7632, 40.2336]  # velocities
     profile_mod_x = [2.2352, 13.4112, 24.5872, 35.7632]  # profile mod speeds, mph: [5., 30., 55., 80.]
 
-    if self.df_manager.is_auto:  # decide which profile to use, model profile will be updated before this
-      df_profile = self.model_profile
-    else:
-      df_profile = self.user_profile
 
-    if df_profile == self.df_profiles.roadtrip:
-      y_dist = [1.3978, 1.4132, 1.4318, 1.4536, 1.485, 1.5229, 1.5819, 1.6203, 1.7238, 1.8231, 1.8379, 1.8495, 1.8535]  # TRs
-      profile_mod_pos = [0.92, 0.7, 0.25, 0.15]
-      profile_mod_neg = [1.1, 1.3, 2.0, 2.3]
-    elif df_profile == self.df_profiles.traffic:  # for in congested traffic
-      x_vel = [0.0, 1.892, 3.7432, 5.8632, 8.0727, 10.7301, 14.343, 17.6275, 22.4049, 28.6752, 34.8858, 40.35]
-      # y_dist = [1.3781, 1.3791, 1.3802, 1.3825, 1.3984, 1.4249, 1.4194, 1.3162, 1.1916, 1.0145, 0.9855, 0.9562]  # original
-      # y_dist = [1.3781, 1.3791, 1.3112, 1.2442, 1.2306, 1.2112, 1.2775, 1.1977, 1.0963, 0.9435, 0.9067, 0.8749]  # avg. 7.3 ft closer from 18 to 90 mph
-      y_dist = [1.3781, 1.3791, 1.3457, 1.3134, 1.3145, 1.318, 1.3485, 1.257, 1.144, 0.979, 0.9461, 0.9156]
-      profile_mod_pos = [1.05, 1.55, 2.6, 3.75]
-      profile_mod_neg = [0.84, .275, 0.1, 0.05]
-    elif df_profile == self.df_profiles.relaxed:  # default to relaxed/stock
-      y_dist = [1.385, 1.394, 1.406, 1.421, 1.444, 1.474, 1.516, 1.534, 1.546, 1.568, 1.579, 1.593, 1.614]
-      profile_mod_pos = [1.0] * 4
-      profile_mod_neg = [1.0] * 4
-    else:
-      raise Exception('Unknown profile type: {}'.format(df_profile))
+    x_vel = [0.0, 1.892, 3.7432, 5.8632, 8.0727, 10.7301, 14.343, 17.6275, 22.4049, 28.6752, 34.8858, 40.35]
+    y_dist = [1.3781, 1.3791, 1.3457, 1.3134, 1.3145, 1.318, 1.3485, 1.257, 1.144, 0.979, 0.9461, 0.9156]
+    profile_mod_pos = [1.05, 1.55, 2.6, 3.75]
+    profile_mod_neg = [0.84, .275, 0.1, 0.05]
+
 
     # Profile modifications - Designed so that each profile reacts similarly to changing lead dynamics
     profile_mod_pos = interp(self.car_data.v_ego, profile_mod_x, profile_mod_pos)
     profile_mod_neg = interp(self.car_data.v_ego, profile_mod_x, profile_mod_neg)
 
-    sng_TR = 1.8  # reacceleration stop and go TR
-    sng_speed = 18.0 * CV.MPH_TO_MS
-
-    if self.car_data.v_ego > sng_speed:  # keep sng distance until we're above sng speed again
-      self.sng = False
-
-    if (self.car_data.v_ego >= sng_speed or self.df_data.v_egos[0]['v_ego'] >= self.car_data.v_ego) and not self.sng:
-      # if above 15 mph OR we're decelerating to a stop, keep shorter TR. when we reaccelerate, use sng_TR and slowly decrease
-      TR = interp(self.car_data.v_ego, x_vel, y_dist)
-      TR, profile_mod_pos, profile_mod_neg = self.global_profile_mod(TR, profile_mod_pos, profile_mod_neg)  # only within normal driving conditions
-    else:  # this allows us to get closer to the lead car when stopping, while being able to have smooth stop and go when reaccelerating
-      self.sng = True
-      x = [sng_speed * 0.7, sng_speed]  # decrease TR between 12.6 and 18 mph from 1.8s to defined TR above at 18mph while accelerating
-      y = [sng_TR, interp(sng_speed, x_vel, y_dist)]
-      TR = interp(self.car_data.v_ego, x, y)
+    TR = interp(self.car_data.v_ego, x_vel, y_dist)
 
     TR_mods = []
     # Dynamic follow modifications (the secret sauce)
     x = [-26.8224, -20.0288, -15.6871, -11.1965, -7.8645, -4.9472, -3.0541, -2.2244, -1.5045, -0.7908, -0.3196, 0.0, 0.5588, 1.3682, 1.898, 2.7316, 4.4704]  # relative velocity values
     y = [.76, 0.62323, 0.49488, 0.40656, 0.32227, 0.23914, 0.12269, 0.10483, 0.08074, 0.04886, 0.0072, 0.0, -0.05648, -0.0792, -0.15675, -0.23289, -0.315]  # modification values
-    TR_mods.append(interp(self.lead_data.v_lead - self.car_data.v_ego, x, y))
+    TR_mod = interp(self.lead_data.v_lead - self.car_data.v_ego, x, y) * (1 / 10)
+    self.TR_mod += TR_mod
+    TR += self.TR_mod
 
-    x = [-4.4795, -2.8122, -1.5727, -1.1129, -0.6611, -0.2692, 0.0, 0.1466, 0.5144, 0.6903, 0.9302]  # lead acceleration values
-    y = [0.24, 0.16, 0.092, 0.0515, 0.0305, 0.022, 0.0, -0.0153, -0.042, -0.053, -0.059]  # modification values
-    TR_mods.append(interp(self.lead_data.a_lead, x, y))
 
-    rel_accel_mod = self._calculate_relative_accel_new()
-    if rel_accel_mod is not None:  # if available
-      deadzone = 2 * CV.MPH_TO_MS
-      if self.lead_data.v_lead - deadzone > self.car_data.v_ego:
-       TR_mods.append(rel_accel_mod)
 
-    x = [sng_speed / 5.0, sng_speed]  # as we approach 0, apply x% more distance
-    y = [1.05, 1.0]
-    profile_mod_pos *= interp(self.car_data.v_ego, x, y)  # but only for currently positive mods
-
-    TR_mod = sum([mod * profile_mod_neg if mod < 0 else mod * profile_mod_pos for mod in TR_mods])  # alter TR modification according to profile
-    TR += TR_mod
-
-    if self.car_data.left_blinker or self.car_data.right_blinker and df_profile != self.df_profiles.traffic:
-      x = [8.9408, 22.352, 31.2928]  # 20, 50, 70 mph
-      y = [1.0, .75, .65]  # reduce TR when changing lanes
-      TR *= interp(self.car_data.v_ego, x, y)
     return clip(TR, 0.9, 2.7)
 
   def update_lead(self, v_lead=None, a_lead=None, x_lead=None, status=False, new_lead=False):
