d29b6a4e087a98ea5cbda51f95a7e04d47905e3f
Shane <shane@smiskol.com>
2020-05-29 21:57:31 -0500

    debug

--- helper_scripts/testing/testing_df_fix.py
+++ helper_scripts/testing/testing_df_fix.py
@@ -2,5 +2,5 @@ from cereal import car
 
 actuators = car.CarControl.Actuators.new_message()
 
-actuators.gas = 0.542584255
+actuators.gas = 0.542584255542584255542584255542584255542584255542584255542584255
 print('success!')
