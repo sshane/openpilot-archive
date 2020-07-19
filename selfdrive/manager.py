5c9d96c1d32987ab1458880e7097ec3c1a80dc6e
Shane Smiskol <shane@smiskol.com>
2020-07-19 00:33:58 -0500

    it seemed to work

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -480,7 +480,7 @@ def manager_thread():
 
     if msg.thermal.freeSpace < 0.05:
       logger_dead = True
-    run_all = True
+    run_all = False
     if (msg.thermal.started and "driverview" not in running) or run_all:
       for p in car_started_processes:
         if p == "loggerd" and logger_dead:
