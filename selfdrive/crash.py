d275f3cc017c4f049f7d932fa9cc432bf48a2fee
Shane Smiskol <shane@smiskol.com>
2020-07-31 22:50:43 -0500

    Crash logging to /data/community/crashes (#139)
    * specify username undefined
    
    * test
    
    * forgot to cause error
    
    * test
    
    * test
    
    * test
    
    * test
    
    * log exception to file
    
    * log exception to file
    
    * log exception to file
    
    * move function
    
    * test logic
    
    * works fine
    
    * test
    
    * fix
    
    * test
    
    * fix
    
    * fix

--- selfdrive/crash.py
+++ selfdrive/crash.py
@@ -1,15 +1,24 @@
 """Install exception handler for process crash."""
 import os
 import sys
-import threading
 import capnp
+import threading
+import traceback
 from selfdrive.version import version, dirty
 
 from selfdrive.swaglog import cloudlog
 from common.android import ANDROID
-from common.op_params import opParams
 
-op_params = opParams()
+def save_exception(exc_text):
+  i = 0
+  log_file = '{}/{}'.format(CRASHES_DIR, datetime.now().strftime('%d-%m-%Y--%I:%M%p.log'))
+  if os.path.exists(log_file):
+    while os.path.exists(log_file + str(i)):
+      i += 1
+    log_file += str(i)
+  with open(log_file, 'w') as f:
+    f.write(exc_text)
+  print('Logged current crash to {}'.format(log_file))
 
 if os.getenv("NOLOG") or os.getenv("NOCRASH") or not ANDROID:
   def capture_exception(*args, **kwargs):
@@ -27,20 +36,31 @@ else:
   from raven import Client
   from raven.transport.http import HTTPTransport
   from selfdrive.version import origin, branch, commit
+  from common.op_params import opParams
+  from datetime import datetime
+
+  COMMUNITY_DIR = '/data/community'
+  CRASHES_DIR = '{}/crashes'.format(COMMUNITY_DIR)
+
+  if not os.path.exists(COMMUNITY_DIR):
+    os.mkdir(COMMUNITY_DIR)
+  if not os.path.exists(CRASHES_DIR):
+    os.mkdir(CRASHES_DIR)
 
   error_tags = {'dirty': dirty, 'origin': origin, 'branch': branch, 'commit': commit}
-  username = op_params.get('username', None)
-  if username is not None and isinstance(username, str):
-    error_tags['username'] = username
+  username = opParams().get('username', None)
+  if username is None or not isinstance(username, str):
+    username = 'undefined'
+  error_tags['username'] = username
 
+  sentry_uri = 'https://1994756b5e6f41cf939a4c65de45f4f2:cefebaf3a8aa40d182609785f7189bd7@app.getsentry.com/77924'  # stock
   if 'github.com/shanesmiskol' in origin.lower():  # only send errors if my fork
     sentry_uri = 'https://7f66878806a948f9a8b52b0fe7781201@o237581.ingest.sentry.io/5252098'
-  else:  # else use stock
-    sentry_uri = 'https://1994756b5e6f41cf939a4c65de45f4f2:cefebaf3a8aa40d182609785f7189bd7@app.getsentry.com/77924'
   client = Client(sentry_uri,
                   install_sys_hook=False, transport=HTTPTransport, release=version, tags=error_tags)
 
   def capture_exception(*args, **kwargs):
+    save_exception(traceback.format_exc())
     exc_info = sys.exc_info()
     if not exc_info[0] is capnp.lib.capnp.KjException:
       client.captureException(*args, **kwargs)
