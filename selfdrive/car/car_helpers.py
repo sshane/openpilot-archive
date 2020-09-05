3ed34546396d14a23c5e2aac4ff54547328fbb4c
Shane Smiskol <shane@smiskol.com>
2020-09-04 22:49:19 -0500

    test

--- selfdrive/car/car_helpers.py
+++ selfdrive/car/car_helpers.py
@@ -159,6 +159,7 @@ def fingerprint(logcan, sendcan, has_relay):
     source = car.CarParams.FingerprintSource.fixed
 
   cloudlog.warning("fingerprinted %s", car_fingerprint)
+  cloudlog.warning("fingerprint: {}".format(finger))
   return car_fingerprint, finger, vin, car_fw, source
 
 
