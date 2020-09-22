91f5871dedfb5081450852a4aad3920b38cc339f
Shane Smiskol <shane@smiskol.com>
2020-09-22 15:42:03 -0500

    new moedl

--- selfdrive/controls/lib/dynamic_follow/auto_df_v2/auto_df_v2.py
+++ selfdrive/controls/lib/dynamic_follow/auto_df_v2/auto_df_v2.py
@@ -4,7 +4,7 @@
 
 import numpy as np
 
-wb = np.load('/data/openpilot/selfdrive/controls/lib/dynamic_follow/auto_df_v2/df_model_v2_weights.npz', allow_pickle=True)
+wb = np.load('/data/openpilot/selfdrive/controls/lib/dynamic_follow/auto_df_v2/auto_df_v2_weights.npz', allow_pickle=True)
 w, b = wb['wb']
 
 def predict(x):
@@ -16,6 +16,4 @@ def predict(x):
   l2 = np.dot(l1, w[2]) + b[2]
   l2 = np.maximum(0, l2)
   l3 = np.dot(l2, w[3]) + b[3]
-  l3 = np.maximum(0, l3)
-  l4 = np.dot(l3, w[4]) + b[4]
-  return l4
+  return l3
