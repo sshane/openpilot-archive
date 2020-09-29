bac8297f3684da64e888363917fc27c025340418
Shane Smiskol <shane@smiskol.com>
2020-09-29 01:36:20 -0500

    debug

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -677,7 +677,7 @@ static void SA_ui_draw_df_button(UIState *s) {
 static void SA_ui_draw_driving_options(UIState *s) {
   int btn_w = 207;
   int btn_h = 500;
-  int btn_x = 1920 - (138+10);  // coordinates for center of item
+  int btn_x = 1920 - 138;  // coordinates for center of item
   int btn_y = 1080 / 2;
 //  btn_x = btn_x - btn_w / 2;
   btn_y = btn_y - btn_h / 2;
