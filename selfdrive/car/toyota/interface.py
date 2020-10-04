163d6af84aac1a02c5c808c6247e3e6fc85cdb5c
Shane Smiskol <shane@smiskol.com>
2020-10-03 20:11:32 -0500

    Support for Prius 2021 TSS2 (#214)
    * Support for Prius 2021 TSS2
    
    * add Prius 2019 with ZSS fp key
    
    * add debugging
    
    * fixK
    
    * Revert "add Prius 2019 with ZSS fp key"
    
    This reverts commit b331dd870bbdef1ec2320efb463cdceb5849f35c.
    
    * remove
    
    * Use higher curb weight
    
    * Add 2020 Prius from Trae
    
    * Add 2020 Prius from Trae
    
    * Add 2020 Prius from Trae
    
    * increase time constant
    
    * fix dbc
    
    * tune indi for tss2 prius
    
    * prius 2021 dynamic_gas support (#218)
    
    * prius 2021 dynamic_gas support
    
    * add prius 2020
    
    Co-authored-by: Shane Smiskol <shane@smiskol.com>
    
    * remove PRIUS_2020
    
    * add comment
    
    * rm
    
    * Add PID tune for 2021 TSS2 Prius
    
    * maybe we shouldn't re-init
    
    * eon running too hot. (#219)
    
    * add tss2 non-pedal dg tune
    
    * lets try corolla tune. cus that was it was for rav and i liked that. (#224)
    
    * 20% less gas for tss2 prius
    
    * tune Prius TSS2
    
    * fully set new safetyparam
    
    * Tune TSS2 Prius
    
    * update safetyparam and remove indi
    
    * clean up
    
    * revert thermald changes
    
    * revert thermald changes
    
    * revert dg
    
    Co-authored-by: Kumar <36933347+rav4kumar@users.noreply.github.com>

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -343,6 +343,20 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.6], [0.1]]
       ret.lateralTuning.pid.kf = 0.00006
 
+    elif candidate == CAR.PRIUS_TSS2:
+      stop_and_go = True
+      ret.safetyParam = 50
+      ret.wheelbase = 2.70
+      ret.steerRatio = 13.4  # unknown end-to-end spec
+      tire_stiffness_factor = 0.6371  # hand-tune
+      ret.steerActuatorDelay = 0.55
+      ret.mass = 3115. * CV.LB_TO_KG + STD_CARGO_KG
+
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.5], [0.15]]
+      ret.lateralTuning.pid.kdBP = [0.]
+      ret.lateralTuning.pid.kdV = [2.]
+      ret.lateralTuning.pid.kf = 0.00007818594
+
     ret.steerRateCost = 0.5 if ret.hasZss else 1.0
     ret.centerToFront = ret.wheelbase * 0.44
 
