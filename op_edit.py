2a8e2119afe36fe2fd0099cce7ef07976bcf82bd
Shane Smiskol <shane@smiskol.com>
2020-10-07 16:29:35 -0500

    try this

--- op_edit.py
+++ op_edit.py
@@ -168,7 +168,7 @@ class opEdit:  # use by running `python /data/openpilot/op_edit.py`
       self.info('Current value: {} (type: {})'.format(old_value, type(old_value).__name__), sleep_time=0)
 
       while True:
-        self.prompt('Enter your new value:')
+        self.prompt('\nEnter your new value:')
         new_value = input('>> ').strip()
         if new_value == '':
           self.info('Exiting this parameter...\n', 0.5)
