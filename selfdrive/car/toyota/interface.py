35efc88f3a43c41661aa24deacf546d1e73fa0e1
Shane Smiskol <shane@smiskol.com>
2020-10-07 21:46:53 -0500

    60% of stock ff, this should be even better

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -144,7 +144,7 @@ class CarInterface(CarInterfaceBase):
       else:
         ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.15], [0.035]]
         ret.lateralTuning.pid.kdV = [0.68]
-        ret.lateralTuning.pid.kf = 0.0000225   # full torque for 20 deg at 80mph means 0.00007818594
+        ret.lateralTuning.pid.kf = 0.0000195   # full torque for 20 deg at 80mph means 0.00007818594
 
     elif candidate == CAR.LEXUS_RX:
       stop_and_go = True
