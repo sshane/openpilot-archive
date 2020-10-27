2d4e94d78b0398a9f88c6bc68b67b3e592230d37
Shane Smiskol <shane@smiskol.com>
2020-10-27 03:25:52 -0500

    Allow gas press (on param enable)

--- selfdrive/boardd/boardd.cc
+++ selfdrive/boardd/boardd.cc
@@ -107,6 +107,9 @@ void safety_setter_thread() {
   cereal::CarParams::Reader car_params = cmsg.getRoot<cereal::CarParams>();
   cereal::CarParams::SafetyModel safety_model = car_params.getSafetyModel();
 
+  LOGW("setting unsafe_mode for gas press");
+  panda->set_unsafe_mode(1);  // see safety_declarations.h for allowed values
+
   auto safety_param = car_params.getSafetyParam();
   LOGW("setting safety model: %d with param %d", (int)safety_model, safety_param);
 
