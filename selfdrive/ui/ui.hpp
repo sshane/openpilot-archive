da673c02a871aac847304b0dfe393c5256bb30c5
Shane Smiskol <shane@smiskol.com>
2020-12-08 00:23:42 -0600

    UI logic

--- selfdrive/ui/ui.hpp
+++ selfdrive/ui/ui.hpp
@@ -91,6 +91,10 @@ typedef struct {
 
 typedef struct UIScene {
 
+  int dfButtonStatus;
+  int lsButtonStatus;
+  bool mlButtonEnabled;
+
   mat4 extrinsic_matrix;      // Last row is 0 so we can use mat4.
   bool world_objects_visible;
 
@@ -165,6 +169,7 @@ typedef struct UIState {
   int img_network[6];
 
   SubMaster *sm;
+  PubMaster *pm;
 
   Sound *sound;
   UIStatus status;
@@ -194,6 +199,7 @@ typedef struct UIState {
   bool ignition;
   bool is_metric;
   bool longitudinal_control;
+  bool ui_debug;
   uint64_t last_athena_ping;
   uint64_t started_frame;
 
@@ -208,6 +214,7 @@ typedef struct UIState {
 } UIState;
 
 void ui_init(UIState *s);
+void sa_init(UIState *s, bool full_init);
 void ui_update(UIState *s);
 
 int write_param_float(float param, const char* param_name, bool persistent_param = false);
