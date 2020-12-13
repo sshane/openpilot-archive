e26901ebdde6681bd6bdfd8280c7258cf0d610b9
Shane Smiskol <shane@smiskol.com>
2020-12-13 00:30:19 -0600

    Forgot modelV2

--- selfdrive/controls/plannerd.py
+++ selfdrive/controls/plannerd.py
@@ -23,7 +23,7 @@ def plannerd_thread(sm=None, pm=None):
   VM = VehicleModel(CP)
 
   if sm is None:
-    sm = messaging.SubMaster(['carState', 'controlsState', 'radarState', 'model', 'liveParameters', 'modelLongButton'],
+    sm = messaging.SubMaster(['carState', 'controlsState', 'radarState', 'model', 'liveParameters', 'modelLongButton', 'modelV2'],
                              poll=['radarState', 'model'])
 
   if pm is None:
