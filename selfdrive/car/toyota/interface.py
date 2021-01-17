04c6c437b4b37990fa244094717fb36af341099e
Shane Smiskol <shane@smiskol.com>
2021-01-17 00:39:01 -0600

    Update interface.py (#312)
    Updated corollatss2 indi tune. Tighter below 40mph thanks to briskspirit.
    
    Co-authored-by: birdman6450 <67883352+birdman6450@users.noreply.github.com>

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -272,13 +272,13 @@ class CarInterface(CarInterfaceBase):
       if corollaTSS2_use_indi:  # birdman6450#7399's Corolla 2020 TSS2 Tune
         ret.lateralTuning.init('indi')
         ret.lateralTuning.indi.innerLoopGainBP = [18, 22, 26]
-        ret.lateralTuning.indi.innerLoopGainV = [5, 12, 15]
+        ret.lateralTuning.indi.innerLoopGainV = [9, 12, 15]
         ret.lateralTuning.indi.outerLoopGainBP = [18, 22, 26]
-        ret.lateralTuning.indi.outerLoopGainV = [4, 11, 14.99]
+        ret.lateralTuning.indi.outerLoopGainV = [8, 11, 14.99]
         ret.lateralTuning.indi.timeConstantBP = [18, 22, 26]
-        ret.lateralTuning.indi.timeConstantV = [2, 4, 5.5]
+        ret.lateralTuning.indi.timeConstantV = [1, 3, 4.5]
         ret.lateralTuning.indi.actuatorEffectivenessBP = [18, 22, 26]
-        ret.lateralTuning.indi.actuatorEffectivenessV = [5, 12, 15]
+        ret.lateralTuning.indi.actuatorEffectivenessV = [9, 12, 15]
         ret.steerActuatorDelay = 0.45  # needs verification
       else:
         ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.6], [0.1]]
